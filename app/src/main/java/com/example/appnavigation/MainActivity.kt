a4536c6f8572ea131222381c9327fa539ab14eea
Helmer Capassola <helmercap@gmail.com>
2024-12-01 23:33:25 +0100

    add dependencies and create screen details.

--- app/src/main/java/com/example/appnavigation/MainActivity.kt
+++ app/src/main/java/com/example/appnavigation/MainActivity.kt
@@ -17,6 +17,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.appnavigation.ui.theme.AppNavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppNavigationTheme {
                 //Navegação gerenciada pelo NavHost
-                HomeScreen()
+                NavigationApp()
             }
         }
     }
@@ -34,11 +38,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NavigationApp(){
+    // criar NavController para controlar a navegação
+    val navController = rememberNavController()
 
+    //Define o NavHost com os Destinos
+    NavHost(navController = navController, startDestination = "home"){
+        composable("home") { HomeScreen(navController)  }
+        composable("details") { DetailsScreen(navController)  }
+    }
 }
 
 @Composable
-fun HomeScreen(){
+fun HomeScreen(navController: NavController){
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -46,8 +57,27 @@ fun HomeScreen(){
     ) {
         Text(text = "Tela Inicial", style = MaterialTheme.typography.headlineMedium)
         Spacer(modifier = Modifier.height(20.dp))
-        Button(onClick = {}) {
+        Button(onClick = { navController.navigate("details/123") }) {
             Text(text = "Avançar")
         }
+    }
+}
+
+@Composable
+fun DetailsScreen(navController: NavController){
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)
+    ) {
+
+        Text(text = "Tela de detalhes", style = MaterialTheme.typography.headlineMedium)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview(){
+    AppNavigationTheme {
+        HomeScreen(navController = rememberNavController())
     }
 }
